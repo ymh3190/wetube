5808cd0b49f9a27ff029eaac0046857edfb13591
ymh3190 <59950687+ymh3190@users.noreply.github.com>
2021-02-24 09:35:56 +0900

    ~3.7

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -6,12 +6,13 @@ import {
   postUpload,
   videoDetail,
 } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 videoRouter.get(routes.videoDetail(), videoDetail);
