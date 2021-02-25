ff1a993ca6eb3c83e53401221a79425204f1a268
ymh3190 <59950687+ymh3190@users.noreply.github.com>
2021-02-25 14:46:06 +0900

    ~3.12

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import {
   deleteVideo,
-  editVideo,
+  getEditVideo,
   getUpload,
+  postEditVideo,
   postUpload,
   videoDetail,
 } from "../controllers/videoController";
@@ -11,10 +12,18 @@ import routes from "../routes";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
