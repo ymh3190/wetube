2ad4134872d6eccbb562223cb3ac83637fb514f2
ymh3190 <59950687+ymh3190@users.noreply.github.com>
2021-03-02 23:49:26 +0900

    ~4.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,63 @@
 const path = require("path");
-const ExtractCSS = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
-  moduel: {
+  module: {
     rules: [
       {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
         test: /\.(scss)$/,
-        use: ExtractCSS.extract([
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
           {
             loader: "css-loader",
           },
           {
             loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
+                    {
+                      browserslist: "cover 99.5%",
+                    },
+                  ],
+                ],
+              },
+            },
           },
           {
             loader: "sass-loader",
           },
-        ]),
+        ],
       },
     ],
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]",
+    filename: "[name].js",
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
 };
 
 module.exports = config;
