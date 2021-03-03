c9e7b62949d1648cc5f9158b2b54ede4fb30d51f
ymh3190 <59950687+ymh3190@users.noreply.github.com>
2021-03-03 14:46:23 +0900

    ~5.2

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ const config = {
                   [
                     "autoprefixer",
                     {
-                      browserslist: "cover 99.5%",
+                      overrideBrowserslist: "cover 99.5%",
                     },
                   ],
                 ],
